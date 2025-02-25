26a5c57419502867b4fb205d4d51a0943cc1ab6d
GarlicGiraffe <agrantparra@gmail.com>
2025-02-25 22:09:50 +0000

    Added photo format changer

--- controllers/interface.py
+++ controllers/interface.py
@@ -5,12 +5,15 @@ ISO_MIN = 50
 ISO_MAX = 12800
 SHUTTER_MIN = 1 / 4000
 SHUTTER_MAX = 30
-APERTURE_MIN = 1.0
+APERTURE_MIN = 1
 APERTURE_MAX = 22.0
 WB_MIN = 2500
 WB_MAX = 10000
 FOCUS_MIN = 1
 FOCUS_MAX = 1000
+
+# Define valid file formats
+FILE_FORMATS = ["JPG", "PNG", "TIFF", "RAW"]
 
 # Define the blueprint for interface routes
 interface_bp = Blueprint('interface', __name__)
@@ -18,13 +21,14 @@ interface_bp = Blueprint('interface', __name__)
 # Route to render the interface
 @interface_bp.route("/interface")
 def interface():
-    # Pass the constants to the template
+    # Pass the constants and file formats to the template
     return render_template("interface.html", 
                            ISO_MIN=ISO_MIN, ISO_MAX=ISO_MAX, 
                            SHUTTER_MIN=SHUTTER_MIN, SHUTTER_MAX=SHUTTER_MAX,
                            APERTURE_MIN=APERTURE_MIN, APERTURE_MAX=APERTURE_MAX,
                            WB_MIN=WB_MIN, WB_MAX=WB_MAX,
-                           FOCUS_MIN=FOCUS_MIN, FOCUS_MAX=FOCUS_MAX)
+                           FOCUS_MIN=FOCUS_MIN, FOCUS_MAX=FOCUS_MAX,
+                           FILE_FORMATS=FILE_FORMATS)
 
 # Route to handle AJAX requests and update camera settings
 @interface_bp.route("/update_camera_setting", methods=["POST"])
