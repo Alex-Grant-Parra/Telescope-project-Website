f795c8075d162b64f0acc146cf4df2dd91c3a8f5
GarlicGiraffe <agrantparra@gmail.com>
2025-03-10 20:55:33 +0000

    ooped

--- NEA project.py
+++ NEA project.py
@@ -1,138 +1,197 @@
 from math import sin, cos, tan, asin, acos, atan, atan2, degrees, radians, pi, floor, sqrt
 
+class convert:
 
-# Explaining values:
-# Azimuth = degrees round from north
-# Elevation = Degrees from horizon to zenith
-# Right ascention = Longitude in the sky
-# Declination = Latitude in the sky
-  
-def convertHorizonToEquatorial(LL_AZ, LL_ELV, LR_latitude, LR_LST):
+    @staticmethod
+    def convertHorizonToEquatorial(LL_AZ, LL_ELV, LR_latitude, LR_LST):
 
-    
+        LR_latitudeRAD = radians(LR_latitude)
+        LR_LSTRAD = radians(LR_LST)
 
-    LR_latitudeRAD = radians(LR_latitude)
-    LR_LSTRAD = radians(LR_LST)
+        LR_AZ = radians(convert.HrMinSecToDegrees(LL_AZ[0], LL_AZ[1], LL_AZ[2])) # Input Azimith
+        LR_ELV = radians(convert.HrMinSecToDegrees(LL_ELV[0], LL_ELV[1], LL_ELV[2])) # Input Elevation
 
-    LR_AZ = radians(convertToDecimalDegrees(LL_AZ[0], LL_AZ[1], LL_AZ[2])) # Input Azimith
-    LR_ELV = radians(convertToDecimalDegrees(LL_ELV[0], LL_ELV[1], LL_ELV[2])) # Input Elevation
+        # Finds the declination
+        LR_DEC = asin(sin(LR_ELV)*sin(LR_latitudeRAD) + cos(LR_ELV)*cos(LR_latitudeRAD)*cos(LR_AZ))
 
-    # Finds the declination
-    LR_DEC = asin(sin(LR_ELV)*sin(LR_latitudeRAD) + cos(LR_ELV)*cos(LR_latitudeRAD)*cos(LR_AZ))
+        # Finds the hour angle
+        LR_H = acos((sin(LR_ELV)-sin(LR_latitudeRAD)*sin(LR_DEC)) / (cos(LR_latitudeRAD)*cos(LR_DEC)))/15
 
-    # Finds the hour angle
-    LR_H = acos((sin(LR_ELV)-sin(LR_latitudeRAD)*sin(LR_DEC)) / (cos(LR_latitudeRAD)*cos(LR_DEC)))/15
+        if degrees(sin(LR_AZ)) > 0:
+            LR_H = 360 - LR_H
 
-    if degrees(sin(LR_AZ)) > 0:
-        LR_H = 360 - LR_H
+        # Finds right ascension
+        LR_RA = (LR_LSTRAD - LR_H)%radians(24) # Calculates the right ascention
+        return(convert.DecimalToHrMinSec(degrees(LR_RA)), convert.DecimalToHrMinSec(degrees(LR_DEC))) # Returns a tuple of (Right Ascention, Declination)
 
-    # Finds right ascension
-    LR_RA = (LR_LSTRAD - LR_H)%radians(24) # Calculates the right ascention
-    return(convertToHrMinSec(degrees(LR_RA)), convertToHrMinSec(degrees(LR_DEC))) # Returns a tuple of (Right Ascention, Declination)
+    @staticmethod
+    def convertEquatorialToHorizon(LL_RA, LL_DEC, LR_latitude, LR_LST):
 
-def convertEquatorialToHorizon(LL_RA, LL_DEC, LR_latitude, LR_LST):
+        def getHourAngle(LL_RA, LR_LST): # Getting hour angle, all units in degrees
+            LR_RA = convert.HrMinSecToDegrees(LL_RA[0], LL_RA[1], LL_RA[2])
+            LR_H = LR_LST - LR_RA
 
-    def getHourAngle(LL_RA, LR_LST): # Getting hour angle, all units in degrees
-        LR_RA = convertToDecimalDegrees(LL_RA[0], LL_RA[1], LL_RA[2])
-        LR_H = LR_LST - LR_RA
+            if LR_H < 0:
+                LR_H = LR_H + 24
+            return LR_H
+             
+        LR_latitude_RAD = radians(LR_latitude)
+        LR_H_RAD = radians(getHourAngle(LL_RA, LR_LST)*15)
+        LR_DEC_RAD = radians(convert.HrMinSecToDegrees(LL_DEC[0], LL_DEC[1], LL_DEC[2]))
+        # print(getHourAngle(LL_RA, LR_LST))
+        # print(degrees(LR_H_RAD))
+        LR_ELV_RAD = asin(sin(LR_DEC_RAD) * sin(LR_latitude_RAD) + cos(LR_DEC_RAD) * cos(LR_latitude_RAD) * cos(LR_H_RAD))
 
-        if LR_H < 0:
-            LR_H = LR_H + 24
-        return LR_H
-    
-    
-    LR_latitude_RAD = radians(LR_latitude)
-    LR_H_RAD = radians(getHourAngle(LL_RA, LR_LST)*15)
-    LR_DEC_RAD = radians(convertToDecimalDegrees(LL_DEC[0], LL_DEC[1], LL_DEC[2]))
-    # print(getHourAngle(LL_RA, LR_LST))
-    # print(degrees(LR_H_RAD))
-    LR_ELV_RAD = asin(sin(LR_DEC_RAD) * sin(LR_latitude_RAD) + cos(LR_DEC_RAD) * cos(LR_latitude_RAD) * cos(LR_H_RAD))
+        LR_AZ_RAD = acos((sin(LR_DEC_RAD) - sin(LR_latitude_RAD)*sin(LR_ELV_RAD)) / (cos(LR_latitude_RAD)*cos(LR_ELV_RAD)))
 
-    LR_AZ_RAD = acos((sin(LR_DEC_RAD) - sin(LR_latitude_RAD)*sin(LR_ELV_RAD)) / (cos(LR_latitude_RAD)*cos(LR_ELV_RAD)))
+        if sin(LR_H_RAD) > 0:
+            LR_AZ_DEG = 360 - degrees(LR_AZ_RAD)
+        else:
+            LR_AZ_DEG = degrees(LR_AZ_RAD)
 
-    if sin(LR_H_RAD) > 0:
-        LR_AZ_DEG = 360 - degrees(LR_AZ_RAD)
-    else:
-        LR_AZ_DEG = degrees(LR_AZ_RAD)
+        LR_ELV_DEG = degrees(LR_ELV_RAD)
 
-    LR_ELV_DEG = degrees(LR_ELV_RAD)
+        return(convert.DecimalToHrMinSec(LR_AZ_DEG), convert.DecimalToHrMinSec(LR_ELV_DEG)) # Returns a tuple of (Azimuth, Elevation)
 
-    return(convertToHrMinSec(LR_AZ_DEG), convertToHrMinSec(LR_ELV_DEG)) # Returns a tuple of (Azimuth, Elevation)
+    @staticmethod
+    def DecimalToHrMinSec(LR_hoursSigned):
+        if LR_hoursSigned < 0:
+            sign = "negative"
+        else:
+            sign = "positive"
+        LR_hours = abs(LR_hoursSigned)
+        LI_hours = int(LR_hours)
+        LR_minutes = (LR_hours-int(LR_hours)) * 60
+        LR_seconds = (LR_minutes-int(LR_minutes))*60
+        LI_minutes = int(LR_minutes)
+        result = [LI_hours, LI_minutes, round(LR_seconds, 2)]
+        if sign == "negative":
+            result[0] = result[0]*-1
+        return result
 
-def getJD(LI_yearA, LI_monthA, LI_day):
+    @staticmethod
+    def EquatorialToEcliptic(LL_RA, LL_DEC, LR_AxialTilt):
+        LR_AxialTiltRad = radians(LR_AxialTilt)
 
-    # This gets the Julian date
-    LI_yearB = 0
-    LI_monthB = 0
-    if LI_monthA == 1 or LI_monthA == 2:
-        LI_yearB = LI_yearA - 1
-        LI_monthB = LI_monthA + 12
-    else:
-        LI_yearB = LI_yearA
-        LI_monthB = LI_monthA
+        LR_RA = convert.HrMinSecToDegrees(LL_RA[0], LL_RA[1], LL_RA[2])
+        LR_DEC = convert.HrMinSecToDegrees(LL_DEC[0], LL_DEC[1], LL_DEC[2])
 
-    if [LI_yearA, LI_monthA, LI_day] > [1582, 10, 15]: # October 15th 1582 was the day the gregorian calander was implemented
-        LI_A = int(LI_yearB/100)
-        LI_B = 2 - LI_A + int(LI_A/4)
-    else:
-        LI_B = 0
+        LR_RA = radians(LR_RA*15)
+        LR_DEC = radians(LR_DEC)
 
-    if LI_yearB < 0:
-        LI_C = int((365.25*LI_yearB) - 0.75)
-    else:
-        LI_C = int(365.25*LI_yearB)
+        LR_EclLat = asin(sin(LR_DEC)*cos(LR_AxialTiltRad) - cos(LR_DEC)*sin(LR_AxialTiltRad)*sin(LR_RA))
 
-    LI_D = int(30.6001*(LI_monthB+1))
+        LR_Y = sin(LR_RA)*cos(LR_AxialTiltRad) + tan(LR_DEC)*sin(LR_AxialTiltRad)
+        LR_X = cos(LR_RA)
 
-    LR_julianDate = LI_B + LI_C + LI_D + LI_day + 1720994.5
+        LR_EclLong = atan2(LR_Y, LR_X)
 
-    return LR_julianDate
+        LR_EclLong = convert.DecimalToHrMinSec(degrees(LR_EclLong))
+        LR_EclLat = convert.DecimalToHrMinSec(degrees(LR_EclLat))
 
-def getGST(LR_julianDate, LI_hour, LI_minute, LR_second):
+        return (LR_EclLat, LR_EclLong)
 
-    # Finding Greenwich siderial time (GST)
-    LR_S = LR_julianDate - 2451545.0
-    LR_T = LR_S / 36525.0
-    LR_T0 = 6.697374558 + (2400.051336*LR_T) + (0.000025862*LR_T**2)
-    LR_T0 = LR_T0%24
+    @staticmethod
+    def HrMinSecToDegrees(LI_hour, LI_minute, LR_second):
+        return LI_hour + LI_minute / 60 + LR_second / 3600
 
-    # Finding the UT in decimal hours
-    LR_UTdecimalHoursSeconds = LR_second/60
-    LR_UTdecimalHoursMinutes = (LR_UTdecimalHoursSeconds + LI_minute)/60
-    LR_UTdecimalHours = LR_UTdecimalHoursMinutes + LI_hour
+    @staticmethod
+    def EclipticToEquatorial(LL_EclLat, LL_EclLong, LR_AxialTiltDeg):
 
-    LR_UTdecimalHours = LR_UTdecimalHours*1.002737909
+        LR_AxialTiltRad = radians(LR_AxialTiltDeg)
+        LR_EclLong = radians(convert.HrMinSecToDegrees(LL_EclLong[0], LL_EclLong[1], LL_EclLong[2]))
+        LR_EclLat = radians(convert.HrMinSecToDegrees(LL_EclLat[0], LL_EclLat[1], LL_EclLat[2]))
 
-    LR_GST = (LR_T0 + LR_UTdecimalHours)%24
+        LR_DEC = asin(sin(LR_EclLat)*cos(LR_AxialTiltRad)+cos(LR_EclLat)*sin(LR_AxialTiltRad)*sin(LR_EclLong))
 
-    return LR_GST
+        LR_Y = sin(LR_EclLong)*cos(LR_AxialTiltRad)-tan(LR_EclLat)*sin(LR_AxialTiltRad)
+        LR_X = cos(LR_EclLong)
 
-def getLST(LR_Longitude, LR_GST):
-    LR_Longitude_hours = LR_Longitude/15
-    LR_LST = (LR_GST+LR_Longitude_hours)%24
-    if LR_LST < 0:
-        LR_LST = LR_LST + 24
-    
+        LR_RA = atan2(LR_Y, LR_X)
 
-    return LR_LST
+        LR_RA = (degrees(LR_RA)/15)%24
+        LR_DEC = degrees(LR_DEC)%360
+        # print(f"LR_DEC = {LR_DEC}")
 
-def convertToHrMinSec(LR_hoursSigned):
-    if LR_hoursSigned < 0:
-        sign = "negative"
-    else:
-        sign = "positive"
-    LR_hours = abs(LR_hoursSigned)
-    LI_hours = int(LR_hours)
-    LR_minutes = (LR_hours-int(LR_hours)) * 60
-    LR_seconds = (LR_minutes-int(LR_minutes))*60
-    LI_minutes = int(LR_minutes)
-    result = [LI_hours, LI_minutes, round(LR_seconds, 2)]
-    if sign == "negative":
-        result[0] = result[0]*-1
-    return result
+        LR_RA = convert.DecimalToHrMinSec(LR_RA%24)
 
-def convertToDecimalDegrees(LI_hour, LI_minute, LR_second):
-    return LI_hour + LI_minute / 60 + LR_second / 3600
+        # print(LR_DEC)
+        LR_DEC = LR_DEC%360
+        if LR_DEC > 180:
+            LR_DEC = LR_DEC - 360
+        elif LR_DEC > 90:
+            LR_DEC = 180 - LR_DEC
+
+        LR_DEC = convert.DecimalToHrMinSec(LR_DEC)
+
+        return(LR_RA, LR_DEC)
+
+
+class time:
+
+    @staticmethod
+    def getLST(LR_Longitude, LR_GST):
+        LR_Longitude_hours = LR_Longitude/15
+        LR_LST = (LR_GST+LR_Longitude_hours)%24
+        if LR_LST < 0:
+            LR_LST = LR_LST + 24
+        return LR_LST
+
+    @staticmethod
+    def getGST(LR_julianDate, LI_hour, LI_minute, LR_second):
+        
+        # Finding Greenwich siderial time (GST)
+        LR_S = LR_julianDate - 2451545.0
+        LR_T = LR_S / 36525.0
+        LR_T0 = 6.697374558 + (2400.051336*LR_T) + (0.000025862*LR_T**2)
+        LR_T0 = LR_T0%24
+
+        # Finding the UT in decimal hours
+        LR_UTdecimalHoursSeconds = LR_second/60
+        LR_UTdecimalHoursMinutes = (LR_UTdecimalHoursSeconds + LI_minute)/60
+        LR_UTdecimalHours = LR_UTdecimalHoursMinutes + LI_hour
+
+        LR_UTdecimalHours = LR_UTdecimalHours*1.002737909
+
+        LR_GST = (LR_T0 + LR_UTdecimalHours)%24
+
+        return LR_GST
+
+    @staticmethod
+    def getJD(LI_yearA, LI_monthA, LI_day):
+
+        # This gets the Julian date
+        LI_yearB = 0
+        LI_monthB = 0
+        if LI_monthA == 1 or LI_monthA == 2:
+            LI_yearB = LI_yearA - 1
+            LI_monthB = LI_monthA + 12
+        else:
+            LI_yearB = LI_yearA
+            LI_monthB = LI_monthA
+
+        if [LI_yearA, LI_monthA, LI_day] > [1582, 10, 15]: # October 15th 1582 was the day the gregorian calander was implemented
+            LI_A = int(LI_yearB/100)
+            LI_B = 2 - LI_A + int(LI_A/4)
+        else:
+            LI_B = 0
+
+        if LI_yearB < 0:
+            LI_C = int((365.25*LI_yearB) - 0.75)
+        else:
+            LI_C = int(365.25*LI_yearB)
+
+        LI_D = int(30.6001*(LI_monthB+1))
+
+        LR_julianDate = LI_B + LI_C + LI_D + LI_day + 1720994.5
+
+        return LR_julianDate
+
+
+
+
+
+
 
 def findAxialTilt(LR_julianDate):
     LR_JD = LR_julianDate-2451545.0 # The constant is the JD for J2000 1.5
@@ -149,61 +208,9 @@ GD_SUNDATA = {
     "Angular diameter": 0.533128
 }
 
-def convertEclipticToEquatorial(LL_EclLat, LL_EclLong, LR_AxialTiltDeg):
-
-    # print(f"\nLL_EclLat = {LL_EclLat}")
-    # print(f"LL_EclLong = {LL_EclLong}")
-    # print(f"LR_LR_AxialTiltN = {LR_AxialTiltDeg}")
 
 
-    LR_AxialTiltRad = radians(LR_AxialTiltDeg)
-    LR_EclLong = radians(convertToDecimalDegrees(LL_EclLong[0], LL_EclLong[1], LL_EclLong[2]))
-    LR_EclLat = radians(convertToDecimalDegrees(LL_EclLat[0], LL_EclLat[1], LL_EclLat[2]))
 
-    LR_DEC = asin(sin(LR_EclLat)*cos(LR_AxialTiltRad)+cos(LR_EclLat)*sin(LR_AxialTiltRad)*sin(LR_EclLong))
-
-    LR_Y = sin(LR_EclLong)*cos(LR_AxialTiltRad)-tan(LR_EclLat)*sin(LR_AxialTiltRad)
-    LR_X = cos(LR_EclLong)
-
-    LR_RA = atan2(LR_Y, LR_X)
-
-    LR_RA = (degrees(LR_RA)/15)%24
-    LR_DEC = degrees(LR_DEC)%360
-    # print(f"LR_DEC = {LR_DEC}")
-
-    LR_RA = convertToHrMinSec(LR_RA%24)
-
-    # print(LR_DEC)
-    LR_DEC = LR_DEC%360
-    if LR_DEC > 180:
-        LR_DEC = LR_DEC - 360
-    elif LR_DEC > 90:
-        LR_DEC = 180 - LR_DEC
-
-    LR_DEC = convertToHrMinSec(LR_DEC)
-
-    return(LR_RA, LR_DEC)
-
-def convertEquatorialToEcliptic(LL_RA, LL_DEC, LR_AxialTilt):
-        LR_AxialTiltRad = radians(LR_AxialTilt)
-
-        LR_RA = convertToDecimalDegrees(LL_RA[0], LL_RA[1], LL_RA[2])
-        LR_DEC = convertToDecimalDegrees(LL_DEC[0], LL_DEC[1], LL_DEC[2])
-
-        LR_RA = radians(LR_RA*15)
-        LR_DEC = radians(LR_DEC)
-
-        LR_EclLat = asin(sin(LR_DEC)*cos(LR_AxialTiltRad) - cos(LR_DEC)*sin(LR_AxialTiltRad)*sin(LR_RA))
-
-        LR_Y = sin(LR_RA)*cos(LR_AxialTiltRad) + tan(LR_DEC)*sin(LR_AxialTiltRad)
-        LR_X = cos(LR_RA)
-
-        LR_EclLong = atan2(LR_Y, LR_X)
-
-        LR_EclLong = convertToHrMinSec(degrees(LR_EclLong))
-        LR_EclLat = convertToHrMinSec(degrees(LR_EclLat))
-
-        return (LR_EclLat, LR_EclLong)
 
 def solveKepler(LR_M, LR_e):
     if LR_e < 0.1:
@@ -223,7 +230,7 @@ def solveKepler(LR_M, LR_e):
 def calculateSolarPos(LR_julianDate):
     
     LR_e = GD_SUNDATA.get("Eccentricity")
-    LR_daysBetween = LR_julianDate - getJD(1990, 1, 0)
+    LR_daysBetween = LR_julianDate - time.getJD(1990, 1, 0)
     # print(f"LR_daysBetween = {LR_daysBetween}")
     LR_N = ((360/365.242191)*LR_daysBetween)%360
     # print(f"LR_N = {LR_N}")
@@ -237,7 +244,7 @@ def calculateSolarPos(LR_julianDate):
     # print(f"LR_V = {LR_V}")  
     LR_EclLong = (LR_V + GD_SUNDATA.get("Ecliptic longitude (perigee)"))%360 # correct upto here
     # print(f"LR_EclLong = {LR_EclLong}") 
-    return convertEclipticToEquatorial(convertToHrMinSec(0), convertToHrMinSec(LR_EclLong), findAxialTilt(LR_julianDate))
+    return convert.EclipticToEquatorial(convert.DecimalToHrMinSec(0), convert.DecimalToHrMinSec(LR_EclLong), findAxialTilt(LR_julianDate))
 
 # These four function below are used to refine coordinates for an object
 # They remove precession, nutation, aberation and refraction
@@ -317,40 +324,3 @@ def calculatePlanetPos(LR_julianDate, LL_Data, LL_EarthData):
 # print(calculatePlanetPos(getJD(1988, 11, 22.75), planetary_data[2], planetary_data[3]))
 
 
-
-
-
-
-# Database connection
-
-import sqlite3
-
-db = "Data.db"
-conn = sqlite3.connect(db)
-cursor = conn.cursor()
-commands = []
-
-print(f"Opened connection to {db}")
-
-# Enter command
-sqlCommand = '''
--- Enter command below here
-
-
-
-'''
-cursor.execute(sqlCommand)
-commands.append(sqlCommand)
-
-# Checks if you wish to commit changes to db
-try:
-    print(f"\nHere are commands you have run since last commit:
-          \n{commands}")
-    if bool(input("\nWould you like to commit changes? (True/False) ")) == True:
-        print("Commiting changes")
-        # Commit the changes
-        conn.commit()
-except TypeError:
-    print("Must enter True/False")
-
-print(f"Closing connection to {db}")
